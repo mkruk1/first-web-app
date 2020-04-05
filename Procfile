ca9d5a6825d25d7b0b48bac46693101251d4168a
Michał Kruk <michalkruk199@gmail.com>
2020-04-06 01:30:21 +0200

    bug fixing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI ()
+app = FastAPI()
 
-@app.get ("/")
-
-def root ():
+@app.get("/")
+async def root ():
     return {"message": "Hello World during the coronavirus pandemic"}
