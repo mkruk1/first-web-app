e0af30e2798f5e515142ba3fdfebb48f0d8749fc
Michał Kruk <michalkruk199@gmail.com>
2020-04-06 10:47:36 +0200

    converted to task 2

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Dict
 
 app = FastAPI()
 
 @app.get("/")
 async def root ():
-    return {"message": "Hello World during the coronavirus pandemic!"}
+    return {"method": "Hello World during the coronavirus pandemic!"}
+
+class HelloResp (BaseModel):
+    msg: str
+
+@app.get("/get", response_model = HelloResp)
+async def getMethod (name: str):
+    return HelloResp (msg=f "Hello {name}")
+
+class GiveMeSomethingRq (BaseModel):
+    first_key: str
+
+class GiveMeSomethingRespo (BaseModel):
+    received: Dict
+    constant_data: str = "python jest super"
+
+@app.post("/dej/mi/cos", reponse_model = GiveMeSomethingResp)
+def receive_something (rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp (received = rq.dict())
