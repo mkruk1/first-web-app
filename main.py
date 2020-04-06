4f8e8b0709c6e61367e4787e9ca03233843f0103
Michał Kruk <michalkruk199@gmail.com>
2020-04-06 11:22:43 +0200

    doing task 2

--- main.py
+++ main.py
@@ -8,21 +8,41 @@ app = FastAPI()
 async def root ():
     return {"method": "Hello World during the coronavirus pandemic!"}
 
+@app.get ("/method")
+async def getMethod ():
+    return {"method": "GET"}
 
+@app.post ("/method")
+async def postMethod ():
+    return {"method": "POST"}
+
+
+@app.put ("/method")
+async def putMethod ():
+    return {"method": "PUT"}
+
+
+@app.delete ("/method")
+async def deleteMethod ():
+    return {"method": "DELETE"}
+
+
+"""
 class HelloResp (BaseModel):
     msg: str
 
 @app.get("/method/", response_model = HelloResp)
 async def getMethod (name: str):
-    return HelloResp (msg=f "Hello {name}")
+    return HelloResp (msg="Hello {name}")
 
 class GiveMeSomethingRq (BaseModel):
     first_key: str
 
-class GiveMeSomethingRespo (BaseModel):
+class GiveMeSomethingResp (BaseModel):
     received: Dict
     constant_data: str = "python jest super"
 
-@app.post("/post/", reponse_model = GiveMeSomethingResp)
+@app.post("/post/", response_model = GiveMeSomethingResp)
 def receive_something (rq: GiveMeSomethingRq):
     return GiveMeSomethingResp (received = rq.dict())
+    """
