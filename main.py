495a8dd4b470c8f33b332e21a66c0294cf1f0976
Michał Kruk <michalkruk199@gmail.com>
2020-04-06 16:42:47 +0200

    do it

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
+from starlette.responses import RedirectResponse 
 
 app = FastAPI()
-
-id_number = -1 
-
-@app.get ("/")
-async def showText ():
-    return {"message": "this works7"}
+patients = []
 
 
-class GiveMeSomethingRq (BaseModel):
-    name : str
-    surename : str 
-
-class GiveMeSomethingResp (BaseModel):
-    id : int
-    patient : Dict
-
-@app.post ("/patient", )
-def receive_something (rq: GiveMeSomethingRq):
-    global id_number 
-    id_number += 1
-    return {"id" : id_number, "patient" : rq.dict()}
+class Patient ():
+    def __init__ (self, name, surname):
+        self.name = name
+        self.surname = surname
 
 
-
-
-
+@app.get ("/patient/{pk}")
+def read_item (pk: int):
+    if (pk >= len (patients)):
+        return RedirectResponse (url= 'https://en.wikipedia.org/wiki/List_of_HTTP_status_codes') 
+    return {"name": patients [pk].name, "surname": patients [pk].surname }
---
+++ task3.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import Dict
+
+app = FastAPI()
+
+id_number = -1 
+
+@app.get ("/")
+async def showText ():
+    return {"message": "this works7"}
+
+
+class GiveMeSomethingRq (BaseModel):
+    name : str
+    surename : str 
+
+class GiveMeSomethingResp (BaseModel):
+    id : int
+    patient : Dict
+
+@app.post ("/patient", )
+def receive_something (rq: GiveMeSomethingRq):
+    global id_number 
+    id_number += 1
+    return {"id" : id_number, "patient" : rq.dict()}
+
+
+
+
+
