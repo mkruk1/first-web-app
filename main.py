b274ad5219710f1154877a9614dacbe6c81eed1e
Michał Kruk <michalkruk199@gmail.com>
2020-04-26 14:42:49 +0200

    finally working

--- main.py
+++ main.py
@@ -4,33 +4,29 @@ from typing import Dict
 from starlette.responses import RedirectResponse 
 
 
-class Patient ():
-    def __init__ (self, name, surename):
-        self.name = name
-        self.surname = surename
-
-
 app = FastAPI()
-patients = []
-patients.append (Patient ("ja", "ty"))
+class Patient (BaseModel):
+    name: str
+    surename: str
 
 
 @app.get ("/")
 def justBasicStuff ():
-    return HTTPException (status_code = 203)
+    raise HTTPException (status_code = 203)
+
+
+patients_list = []
 
 @app.get ("/patient/{pk}")
 def findPatient (pk: int):
-    if (pk >= len (patients)):
-        return HTTPException(status_code = 204)
-    return {"name": patients [pk].name, "surename": patients [pk].surename }
+    if (pk >= len (patients_list)):
+        raise HTTPException(status_code = 204)    
+
+    return patients_list [pk]
 
 
-class My_rq (BaseModel):
-    patient : Dict
 
-
-@app.post ("/patient")
-def createPatient (rq: My_rq): 
-    my_dict = rq.dict()
-    patients.append (Patient (my_dict ["name"], my_dict ["surename"]))
+@app.post ("/patient", response_model = Patient)
+def createPatient (patient: Patient): 
+    patients_list.append (patient.dict ())
+    return patient
--- test_main.py
+++ test_main.py
@@ -5,16 +5,24 @@ client = TestClient (app)
 
 def test_read_main ():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 203
+
+
+def test_wrong_url ():
+    reponse = client.get ("/lol/")
+    assert reponse.status_code == 404
 
 
 def test_wrong_patient ():
     response = client.get ("/patient/3")
-    assert response.status_code == 200
+    assert response.status_code == 204
 
 
 def test_add_patient ():
-    client.post ("/patient/3", json = {"name": "andrew", "surename": "duda"})
-    response = client.get ("/patient/3");
-    assert response == {"name": "andrew", "surename": "duda"}
+    response = client.post ("/patient", json = {"name": "andrew", "surename": "duda"})
+    assert response.status_code == 200
+    response = client.get ("/patient/0");
+    assert response.status_code == 200
+    assert response.json () == {"name": "andrew", "surename": "duda"}
+
 
