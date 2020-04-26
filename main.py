0562face247c75b7b9a51bcbad0add1b33e924ce
Michał Kruk <michalkruk199@gmail.com>
2020-04-26 22:03:49 +0200

    maybe now

--- main.py
+++ main.py
@@ -5,25 +5,26 @@ from starlette.responses import RedirectResponse
 import secrets
 
 app = FastAPI ()
-app.tokens = []
+app.sessions= {}
 security = HTTPBasic ()
+
+
+def login_validation (credentials: HTTPBasicCredentials = Depends (security)):
+    correct_username = secrets.compare_digest (credentials.username, 'trudnY')
+    correct_password = secrets.compare_digest (credentials.password, 'PaC13Nt')
+
+    if not (correct_username and correct_password):
+        raise HTTPException (status_code = status.HTTP_401_UNAUTHORIZED, detail = 'Incorrect login or password', headers = {"WWW-Authenticate": "Basic"})
+
+    session_token = sha256 (bytes (f"{credentials.username}{credentials.password}{app.secret_key}", endcoding='utf8')).hexdigest ()
+    app.sessions [session_token] = credentials.username
+    return session_token
+
 
     
 @app.post ("/login")
-def create_cookie (user: str, password: str, response: Response, credentials: HTTPBasicCredentials = Depends (security)):
-    if_correct_username = secrets.compare_digest (credentials.username, "trudnY")
-    if_correct_password = secrets.compare_digest (credentials.password, "PaC13Nt")
-
-    if ((not if_correct_username) and (not if_correct_password)):
-        raise HTTPException (status_code = status.HTTP_401_UNAUTHORISED)
-
-    session_token = sha256 (bytes (f"{user}{password}{app.secret.key}", encoding='utf8'))
-    app.tokens.append (session_token)
-
-    response = RedirectResponse (url = "/welcome")
-    response.set_cookie (key= "session_token", value = session_token)
-
-    return response
+def login (response: Response, session_token: str, credentials = Depends (login_validation)):
+    return RedirectResponse (url = '/welcome')
 
 
 @app.get ("/welcome") 
