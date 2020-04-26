a04d94a6fe05477313fe9826bed2d7a6bbee5b5f
Michał Kruk <michalkruk199@gmail.com>
2020-04-26 17:14:20 +0200

    bug fixed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Dict
 
+app = FastAPI ()
+
 @app.get ("/")
 def basicFunc ():
     return {"message": "Hello world"}
